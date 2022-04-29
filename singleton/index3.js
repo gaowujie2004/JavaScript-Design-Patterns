0f8c775653d13ca5fe7eedafc19bd7994de3e333
GaoWuJie2004 <gaowujie.2004@qq.com>
2022-04-29 17:21:51 +0800

    fix: 单例模式

--- singleton/index3.js
+++ singleton/index3.js
@@ -27,6 +27,26 @@ let a = new ProxySingletonCreateDiv('GaoWuJie');
 let b = new ProxySingletonCreateDiv('QinBeiLei');
 console.log('--相等', a === b, a, b);
 
+/**================================== 动态代理 **/
+var ProxySingletonCustom = function (ConstructionFn) {
+  // 私有变量
+  let instance = null;
+
+  return function (html) {
+    if (!instance) {
+      return (instance = new ConstructionFn(html));
+    }
+
+    return instance;
+  };
+};
+
+var ProxySingletonCreateDiv = ProxySingletonCustom(CreateDiv);
+
+var c = new ProxySingletonCreateDiv('孩子啊');
+var d = new ProxySingletonCreateDiv('孩子啊');
+console.log('--相等', c === d, c, d);
+
 /**
  * 通过引入代理类的方式，我们同样完成了一个单例模式的编写，跟之前不同的是，现在我们把负责管理单例的逻辑移到了
  * 代理类 ProxySingletonCreateDiv 中。这样一来，CreateDiv 就变成了普通类，跟他的 ProxySingletonCreateDiv 组合起来
