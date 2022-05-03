acb1d360c8b80e998cd95b91bac9a69741a27751
GaoWuJie2004 <gaowujie.2004@qq.com>
2022-05-03 16:58:28 +0800

    feat: 策略模式 完结

--- cel/index5.js
+++ cel/index5.js
@@ -1,12 +1,60 @@
 /**================================== 一个字段，多个校验 **/
 
-validator.add(loginForm.userName, [
-  {
-    strategy: 'isNotEmpty',
-    message: '不能为空',
+/********* 策略对象 *********/
+const strategies = {
+  isNotEmpty(val, errMsg) {
+    if (val.trim() === '') {
+      return errMsg;
+    }
   },
-  {
-    strategy: 'minLength:6',
-    message: '用户名长度至少6位',
+  minLength(val, length, errMsg) {
+    if (val.trim().length < length) {
+      return errMsg;
+    }
   },
-]);
+  isMobile(val, errMsg) {
+    if (val.length < 11 || !/1\d{10}/.test(val)) {
+      return errMsg;
+    }
+  },
+};
+
+/********* Validator 类 *********/
+class Validator {
+  constructor() {
+    this.cache = []; // 保存校验规则
+  }
+
+  add(val, rule, errMsg) {
+    const [strategy, length] = rule.split(':');
+    this.cache.push(() => {
+      return strategies[strategy](val, length, errMsg);
+    });
+  }
+
+  start() {
+    for (const fn of this.cache) {
+      if (fn() !== undefined) {
+        return;
+      }
+    }
+  }
+}
+
+/********* 客户端代码 *********/
+function validatorLoginHandler() {
+  const validator = new Validator();
+  validator.add(loginForm.userName);
+  validator.add(loginForm.passWord, 'minLength:6', '密码不能小于6位');
+  validator.add(loginForm.userName, 'isMobile', '手机号码不正确');
+
+  return validator.start(); // 获取校验结果
+}
+
+$('button').onclick = () => {
+  var errMsg = validatorLoginHandler();
+  if (errMsg) {
+    alert(errMsg);
+    return false;
+  }
+};
